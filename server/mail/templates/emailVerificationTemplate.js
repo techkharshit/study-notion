123d561bc298c2147a883a517f665c51d97d70ff
kharshit <155631133+techkharshit@users.noreply.github.com>
2025-01-19 00:26:18 +0530

    Update emailVerificationTemplate.js

--- server/mail/templates/emailVerificationTemplate.js
+++ server/mail/templates/emailVerificationTemplate.js
@@ -66,8 +66,8 @@ const otpTemplate = (otp) => {
 	
 	<body>
 		<div class="container">
-			<a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-					src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
+			<a href="https://study-notion-nine-tau.vercel.app"><img class="logo"
+					src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw4weqxqEww-yOStWXXgSsrIxn4hfkZT3kEg&s" alt="StudyNotion Logo"></a>
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
 				<p>Dear User,</p>
